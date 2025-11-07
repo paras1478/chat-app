345b1e70932acfa01fae4d238ce3503b5bd52fe3
paras1478 <pankaj147878@gmail.com>
2025-11-07 14:53:33 +0530

    Added excelidraw frontend and updated configs

--- apps/excelidraw-frontend/app/canvas/[roomid]/page.tsx
+++ apps/excelidraw-frontend/app/canvas/[roomid]/page.tsx
@@ -1,52 +1,15 @@
 "use client";
 import { useEffect, useRef } from "react";
+import { initDraw } from "../../draw/page";
 
 export default function Canvas() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  const isDrawing = useRef(false); // <-- this stores clicked state
 
   useEffect(() => {
-    const canvas = canvasRef.current;
-    if (!canvas) return;
-    const ctx = canvas.getContext("2d");
-    if (!ctx) return;
-
-    ctx.lineWidth = 2;
-    ctx.strokeStyle = "red";
-
-    const startDrawing = (e: MouseEvent) => {
-      isDrawing.current = true;
-      ctx.beginPath();
-      ctx.moveTo(e.offsetX, e.offsetY);
-    };
-
-    const stopDrawing = () => {
-      isDrawing.current = false;
-      ctx.closePath();
-    };
-
-    const draw = (e: MouseEvent) => {
-      if (!isDrawing.current) return;
-      ctx.lineTo(e.offsetX, e.offsetY);
-      ctx.stroke();
-    };
-
-    // Event listeners
-    canvas.addEventListener("mousedown", startDrawing);
-    canvas.addEventListener("mouseup", stopDrawing);
-    canvas.addEventListener("mousemove", draw);
-
-    // Cleanup listeners when component unmounts
-    return () => {
-      canvas.removeEventListener("mousedown", startDrawing);
-      canvas.removeEventListener("mouseup", stopDrawing);
-      canvas.removeEventListener("mousemove", draw);
-    };
+    if (canvasRef.current) {
+      initDraw(canvasRef.current);
+    }
   }, []);
 
-  return (
-    <div>
-      <canvas ref={canvasRef} width={1000} height={1000} className="border" />
-    </div>
-  );
+  return <canvas ref={canvasRef} width={2000} height={1000} className="border" />;
 }
